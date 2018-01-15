56668a6f0387dc529011e0aca139633ce5e70d80
Lance Kersey <lance.kersey@gmail.com>
2018-01-14 16:53:52 -0800

    Improve turnaround time for notifications
    -Add MP3 caching to reuse MP3's
    -Obtain chromecasts on the network once at start, rather than every
     request

--- GooglePyNotify.py
+++ GooglePyNotify.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
 import time
 import socket
+import os.path
 import pychromecast
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from gtts import gTTS
 
 HOST_NAME = "0.0.0.0"
 HOST_PORT = 80
+
+MP3_CACHE_DIR = "mp3_cache"
+CHROMECASTS = 0
 
 class HttpServer(SimpleHTTPRequestHandler):
             
@@ -23,7 +27,7 @@ class HttpServer(SimpleHTTPRequestHandler):
             preurl, notification = self.path.split("?")
 
             # Add some error handling for chrome looping
-            redir = "<html><head><meta http-equiv='refresh' content='3;url=.\' /></head><body><h1>Notification Sent! <br>"+notification+"</h1></body></html>"
+            redir = "<html><head><meta http-equiv='refresh' content='0;url=.\' /></head><body><h1>Notification Sent! <br>"+notification+"</h1></body></html>"
             print(redir)
 
             self.wfile.write(redir.encode())
@@ -39,7 +43,7 @@ class HttpServer(SimpleHTTPRequestHandler):
         else:
             SimpleHTTPRequestHandler.do_GET(self)
    
-	# POST is for submitting data
+    # POST is for submitting data
     def do_POST(self):
 
         print( "incomming http: ", self.path )
@@ -51,33 +55,46 @@ class HttpServer(SimpleHTTPRequestHandler):
     def notify(self, notification):
         if notification == "":
                 notification = "No+Notification+Data+Recieved"
-              
-        notification = notification.replace("+"," ") # Replace "+" Char in String for Spaces
-        print("Notification Sent.")
-
-        text = gTTS(text=notification, lang='en-uk') # See Google TTS API for more Languages (Note: This may do translation Also - Needs Testing)
-        text.save("Notification.mp3")
         
+        mp3 = MP3_CACHE_DIR + "/" + notification.replace("+","_") + ".mp3"
+        text = notification.replace("+"," ")
+
+        if not os.path.isfile(mp3) :
+            print("Generating MP3...")
+            tts = gTTS(text=text, lang='en-uk') # See Google TTS API for more Languages (Note: This may do translation Also - Needs Testing)
+            tts.save(mp3)
+        else:
+            print("Reusing MP3...")
+
+        print("Sending notification...")
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Pull IP Address for Local HTTP File Serving (Note: This requires an internet connection)
         s.connect(("8.8.8.8", 80))
         ip_add = s.getsockname()[0]
         print (ip_add)
         s.close()
-        self.Cast(ip_add)
+        self.Cast(ip_add, mp3)
+
+        print("Notification Sent.")
+
         return
     
-    def Cast(self, ip_add):
-        chromecasts = pychromecast.get_chromecasts()
-        castdevice = next(cc for cc in chromecasts if cc.device.model_name == "Google Home")
+    def Cast(self, ip_add, mp3):
+        castdevice = next(cc for cc in CHROMECASTS if cc.device.model_name == "Google Home")
         castdevice.wait()
         mediacontroller = castdevice.media_controller # ChromeCast Specific
-        url = "http://"+ip_add+"/"+"Notification.mp3"
+        url = "http://" + ip_add + "/" + mp3
         print (url)
         mediacontroller.play_media(url, 'audio/mp3')
         return
 
-httpServer = HTTPServer((HOST_NAME, HOST_PORT), HttpServer) #HTTP Server Stuff (Python Librarys)
+if not os.path.exists(MP3_CACHE_DIR):
+    os.makedirs(MP3_CACHE_DIR)
+
+print("Getting chromecasts...")
+CHROMECASTS = pychromecast.get_chromecasts()
+
 print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, HOST_PORT))
+httpServer = HTTPServer((HOST_NAME, HOST_PORT), HttpServer) #HTTP Server Stuff (Python Librarys)
 
 try:
     httpServer.serve_forever()
